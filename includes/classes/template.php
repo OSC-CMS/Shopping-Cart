0c7cf5cf50b9e39d847319ea12400f99c33f91d4
John <osccms.service@gmail.com>
2012-06-28 21:43:55 +0400

    Smarty обновлен до версии 3.1.10

--- includes/classes/template.php
+++ includes/classes/template.php
@@ -9,47 +9,48 @@
 #####################################
 */
 
-require_once (_LIB.'smarty/smarty.class.php');
+require_once (_LIB.'smarty/Smarty.class.php');
 
-class osTemplate extends Smarty 
+class osTemplate extends Smarty
 {
-   function osTemplate()
-   {
-        $this->Smarty();
-        $this->template_dir = _THEMES;
-        $this->compile_dir = _CACHE;
-        $this->config_dir   = _LANG;
+	function __construct()
+	{
+		parent::__construct();
+		$this->setTemplateDir(_THEMES);
+		$this->setCompileDir(_CACHE);
+		$this->setConfigDir(_LANG);
+		$this->setCacheDir(_CACHE);
 		$this->compile_check   =  TEMPLATE_COMPILE_CHECK;
-        $this->cache_dir    = _CACHE;
-        $this->plugins_dir = array(_LIB.'smarty/plugins',);
+		$this->setPluginsDir(_LIB.'smarty/plugins');
 
 		$this->assign('index', http_path('catalog'));
+		$this->assign('tpl_dir', _THEMES.CURRENT_TEMPLATE);
 
-		if ( is_file( _THEMES_C.'lang/'.$_SESSION['language_code'].'.conf') )
+		if (is_file(_THEMES_C.'lang/'.$_SESSION['language_code'].'.conf'))
 		{
-		   $this->config_load(_THEMES_C.'lang/'.$_SESSION['language_code'].'.conf');
+			$this->config_load(_THEMES_C.'lang/'.$_SESSION['language_code'].'.conf');
 		}
 
 		global $p;
 		$name = $p->name;
 		$group = $p->group;
-				
-		$array = array('app_name' => 'osTemplate');
+		$array = array(
+			'app_name' => 'osTemplate'
+		);
 		
 		$array = apply_filter('tpl_vars', $array);
-		
+
 		$p->name = $name;
 		$p->group = $group;
 		$p->set_dir();
-		
-		if ( count($array) > 0 )
+
+		if (count($array) > 0)
 		{
-		   foreach ($array as $name => $value)
-		   {
-		       $this->assign($name, $value);
-		   }
+			foreach ($array as $name => $value)
+			{
+				$this->assign($name, $value);
+			}
 		}
-		
-   }
+	}
 }
 ?>
