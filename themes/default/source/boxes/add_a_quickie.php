e3422dc9f40755869b06a666253acd56564dbef4
John <osccms.service@gmail.com>
2012-07-15 17:32:20 +0400

    корректировка шаблона

--- themes/default/source/boxes/add_a_quickie.php
+++ themes/default/source/boxes/add_a_quickie.php
@@ -14,7 +14,7 @@
 
   $box->assign('FORM_ACTION','<form id="quick_add" method="post" action="' . os_href_link(basename($PHP_SELF), os_get_all_get_params(array('action')) . 'action=add_a_quickie', 'NONSSL') . '">');
   
-  $box->assign('INPUT_FIELD',os_draw_input_field('quickie','','size="15" onkeyup="ajaxAddQuickieUp(this);" id="quick_add_quickie"'));
+  $box->assign('INPUT_FIELD',os_draw_input_field('quickie','','size="15" id="quick_add_quickie"'));
   $box->assign('SUBMIT_BUTTON', buttonSubmit('button_add_quick.gif', null, BOX_HEADING_ADD_PRODUCT_ID));
   $box->assign('FORM_END','</form>');
   $box->assign('BOX_CONTENT', $box_content);
