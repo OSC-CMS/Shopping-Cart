56d57e17a77e930750ea8819b7f0ddce06e47fcd
John <osccms.service@gmail.com>
2012-07-22 12:04:45 +0400

    Удален плагин "ЧПУ генератор"

--- modules/plugins/seo_url/ru.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $lang['seo_url_product_generator'] = 'Автогенератор ЧПУ URL для товаров';
-   $lang['seo_url_product_generator_desc'] = 'адреса для товаров будут вида name.html';
-   
-   $lang['seo_url_lang'] = 'С какого языка брать названия товаров для генерации ЧПУ';
-   
-?>
-
