fb597029a5a550417b02e487e9e01c9c582636f6
John <osccms.service@gmail.com>
2012-09-21 09:53:23 +0400

    Вывод в шаблон "хлебных крошек"

--- includes/classes/breadcrumb.php
+++ includes/classes/breadcrumb.php
@@ -31,31 +31,32 @@
             $this->_trail[] = apply_filter('breadcrumb_info', array('title' => $title, 'link' => $link));
         }
 
-        function trail($separator = '') 
+        function trail() 
         {
+			global $osTemplate;
             do_action('breadcrumb_trail');
 
             $array = array();
             $array['trail'] = $this->_trail;
-            $array['separator'] = $separator;
 
             $array = apply_filter('breadcrumb_trail', $array);
-            $separator = $array['separator'];
             $this->_trail = $array['trail'];
 
-            $trail_string = '<ul class="breadcrumb">';
-            for ($i=0, $n=sizeof($this->_trail); $i<$n; $i++) {
-				if (isset($this->_trail[$i]['link']) && os_not_null($this->_trail[$i]['link']) && $i != $n-1) {
-                    $trail_string .= '<li><a href="' . $this->_trail[$i]['link'] . '"><span>'.$this->_trail[$i]['title'].'</span></a></li>';
-                } else {
-                    $trail_string .= '<li><span>'.$this->_trail[$i]['title'].'</span></li>';
-                }
-
-                if (($i+1) < $n) $trail_string .= $separator;
+			$aBreadCrumbs = array();
+            for ($i=0, $n=sizeof($this->_trail); $i<$n; $i++)
+			{
+				// Массив с "хлебными крошками"
+				$aBreadCrumbs[] = $this->_trail[$i];
             }
-			$trail_string .= '</ul>';
 
-            return  apply_filter('trail_string', $trail_string);
+			// Передаем массив в файл шаблона breadcrumb.html
+			$osTemplate->assign('aBreadCrumbs', $aBreadCrumbs);
+
+			// Файл шаблона Не кэшируем
+			$osTemplate->caching = 0;
+			$getTemplate = $osTemplate->fetch(CURRENT_TEMPLATE.'/module/breadcrumb.html');
+
+			return $getTemplate;
         }
     }
 ?>
