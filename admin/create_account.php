6b7899ca2eaf285cf001509988ab8efaf0e24160
John <osccms.service@gmail.com>
2012-11-01 13:12:53 +0400

    при добавлении покупателя по умолчанию выбрана группа Покупатель вместо Админ

--- admin/create_account.php
+++ admin/create_account.php
@@ -766,7 +766,7 @@ if ($error == true) {
 if ($processed == true) {
 	echo os_draw_hidden_field('status');
 } else {
-	echo os_draw_pull_down_menu('status', $customers_statuses_array);
+	echo os_draw_pull_down_menu('status', $customers_statuses_array, 2);
 }
 ?></td>
           </tr>
