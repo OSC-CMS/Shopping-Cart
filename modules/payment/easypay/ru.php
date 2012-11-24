9d91eb4d688556fb013710272123a536b267ddf9
John <osccms.service@gmail.com>
2012-11-24 17:05:27 +0400

    модуль оплаты easypay

---
+++ modules/payment/easypay/ru.php
@@ -0,0 +1,35 @@
+<?php
+/*
+*---------------------------------------------------------
+*
+*	OSC-CMS - Open Source Shopping Cart Software
+*	http://osc-cms.com
+*
+*---------------------------------------------------------
+*	Copyright (c) 2012 VamShop
+*---------------------------------------------------------
+*/
+
+define('MODULE_PAYMENT_EASYPAY_TEXT_TITLE', 'EasyPay');
+define('MODULE_PAYMENT_EASYPAY_TEXT_PUBLIC_TITLE', 'EasyPay');
+define('MODULE_PAYMENT_EASYPAY_TEXT_ADMIN_DESCRIPTION', 'Модуль оплаты EasyPay<br /><br /><b>Как правильно настроить модули: <a href="http://osc-cms.com/docs" target="_blank"><u>здесь</u></a>.</b>');
+define('MODULE_PAYMENT_EASYPAY_TEXT_DESCRIPTION', 'После нажатия кнопки Подтвердить заказ Вы перейдёте на сайт платёжной системы для оплаты заказа, после оплаты Ваш заказ будет выполнен.');
+
+define('MODULE_PAYMENT_EASYPAY_STATUS_TITLE' , 'Разрешить модуль EasyPay');
+define('MODULE_PAYMENT_EASYPAY_STATUS_DESC' , 'Вы хотите разрешить использование модуля при оформлении заказов?');
+define('MODULE_PAYMENT_EASYPAY_ALLOWED_TITLE' , 'Разрешённые страны');
+define('MODULE_PAYMENT_EASYPAY_ALLOWED_DESC' , 'Укажите коды стран, для которых будет доступен данный модуль (например RU,DE (оставьте поле пустым, если хотите что б модуль был доступен покупателям из любых стран))');
+define('MODULE_PAYMENT_EASYPAY_MERCHNO_TITLE' , 'EasyPay ID:');
+define('MODULE_PAYMENT_EASYPAY_MERCHNO_DESC' , 'Номер Поставщика в EasyPay. Выдается Администратором при подключении к EasyPay');
+define('MODULE_PAYMENT_EASYPAY_SORT_ORDER_TITLE' , 'Порядок сортировки');
+define('MODULE_PAYMENT_EASYPAY_SORT_ORDER_DESC' , 'Порядок сортировки модуля.');
+define('MODULE_PAYMENT_EASYPAY_ZONE_TITLE' , 'Зона');
+define('MODULE_PAYMENT_EASYPAY_ZONE_DESC' , 'Если выбрана зона, то данный модуль оплаты будет виден только покупателям из выбранной зоны.');
+define('MODULE_PAYMENT_EASYPAY_WEBKEY_TITLE' , 'Электронный ключ');
+define('MODULE_PAYMENT_EASYPAY_WEBKEY_DESC' , 'Электронный ключ. Выдается Администратором при подключении к EasyPay');
+define('MODULE_PAYMENT_EASYPAY_DEBUG_TITLE' , 'Отладочный режим');
+define('MODULE_PAYMENT_EASYPAY_DEBUG_DESC' , 'Режим отладки (0 или отсутствует – отладочный режим отключен, 1 – включен)');
+define('MODULE_PAYMENT_EASYPAY_ORDER_STATUS_ID_TITLE' , 'Укажите оплаченный статус заказа');
+define('MODULE_PAYMENT_EASYPAY_ORDER_STATUS_ID_DESC' , 'Укажите оплаченный статус заказа.');
+  
+?>
