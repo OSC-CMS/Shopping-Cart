9bf0c2c087ceddcf4850e79776fbe30e7070a830
John <osccms.service@gmail.com>
2013-12-23 23:09:24 +0400

    проверка обновлений

---
+++ admin/update.php
@@ -0,0 +1,58 @@
+<?php
+/*
+*---------------------------------------------------------
+*
+*	CartET - Open Source Shopping Cart Software
+*	http://www.cartet.org
+*
+*---------------------------------------------------------
+*/
+
+require('includes/top.php');
+
+$breadcrumb->add('Update', 'update.php');
+
+$main->head();
+$main->top_menu();
+?>
+
+<?php $update = $cartet->service->checkUpdate(); ?>
+
+<?php if (!empty($update['version'])){ ?>
+
+	<h4>Версия: <?php echo $update['version']; ?></h4>
+	<p class="muted">Дата релиза: <?php echo $update['date']; ?></p>
+
+	<?php if (!function_exists('curl_init')){ ?>
+		<p>
+			Автоматическая загрузка не возможна, поскольку на сервере отсутствует CURL.<br>
+			Скачайте архив с обновлением вручную и установите через <a href="update.php?action=install">установку дополнений</a>
+		</p>
+		<p><a class="btn" href="<?php echo $update['url'];?>">Скачать обновление</a></p>
+
+	<?php } else { ?>
+
+		<div class="alert alert-info">Автоматическое обновление заменит файлы скрипта, что приведет к потере изменений которые вы вносили!</div>
+
+		<p>
+			<a class="btn" href="update.php?action=install" onclick="return installUpdate(this)">Загрузить обновление</a>
+			<span class="load" style="display:none">Загружаю...</span>
+		</p>
+
+		<script>
+			function installUpdate(link)
+			{
+				link = $(link);
+				link.parent('p').addClass('loading').find('.load').show();
+				link.hide();
+				window.location.href = link.attr('href');
+				return false;
+			}
+		</script>
+	<?php } ?>
+
+<?php } ?>
+
+
+
+<?php $main->bottom(); ?>
---
+++ includes/_classes/Service.class.php
@@ -0,0 +1,86 @@
+<?php
+/*
+*---------------------------------------------------------
+*
+*	CartET - Open Source Shopping Cart Software
+*	http://www.cartet.org
+*
+*---------------------------------------------------------
+*/
+
+class apiService extends CartET
+{
+	private $api_url = 'http://cartet.org/api.php';
+
+	private $cache_file = 'update.dat';
+
+	public function __construct()
+	{
+		$this->cache_file = DIR.DIR_FS_CACHE.'/system/'.$this->cache_file;
+	}
+
+	public function checkUpdate($cached = false)
+	{
+		$current_version = '1.1.0';
+
+		$aUpdateInfo = $this->getData($current_version, $cached);
+
+		if (!$aUpdateInfo)
+		{
+			return false;
+		}
+
+		$aUpdateInfo = json_decode($aUpdateInfo, true);
+
+		$new_version = $aUpdateInfo['version'];
+
+		if (version_compare($new_version, $current_version, '<='))
+		{
+			@unlink($this->cache_file);
+			return false;
+		}
+
+		return $aUpdateInfo;
+	}
+
+	public function getData($current_version, $cached)
+	{
+		if (file_exists($this->cache_file))
+			return file_get_contents($this->cache_file);
+		else if ($cached)
+			return false;
+
+		$url = $this->api_url.'?update='.$current_version;
+
+		$data = get_contents_from_url($url);
+
+		if ($data === false)
+		{
+			return false;
+		}
+
+		file_put_contents($this->cache_file, $data);
+
+		return $data;
+	}
+}
+
+function get_contents_from_url($url)
+{
+	$data = @file_get_contents($url);
+
+	if ($data === false)
+	{
+		if (function_exists('curl_init'))
+		{
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_HEADER, false);
+			$data = curl_exec($curl);
+			curl_close($curl);
+		}
+	}
+
+	return $data;
+}
