aaada5382ff1b14e865b1445a406105264ab2f5a
John <osccms.service@gmail.com>
2014-01-12 17:27:32 +0400

    файл локализации для плагина tinymce

---
+++ modules/plugins/editor_tinymce/ru.php
@@ -0,0 +1,13 @@
+<?php
+/*
+*---------------------------------------------------------
+*
+*	CartET - Open Source Shopping Cart Software
+*	http://www.cartet.org
+*
+*---------------------------------------------------------
+*/
+
+$lang = array(
+	'editor_tinymce_selector' => 'Укажите через запятую классы или id текстового поля (например: .textarea_big, .textarea_small)'
+);
