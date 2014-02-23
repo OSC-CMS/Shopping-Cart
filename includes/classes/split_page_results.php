01cd956138ddeacd69fc5628e142d2577f233c8d
John <osccms.service@gmail.com>
2014-02-23 15:47:23 +0400

    постраничная навигация отдельным файлом шаблона

--- includes/classes/split_page_results.php
+++ includes/classes/split_page_results.php
@@ -8,100 +8,152 @@
 *---------------------------------------------------------
 */
 
-class splitPageResults {
-    var $sql_query, $number_of_rows, $current_page_number, $number_of_pages, $number_of_rows_per_page;
+class splitPageResults
+{
+	var $sql_query, $number_of_rows, $current_page_number, $number_of_pages, $number_of_rows_per_page;
 
-    function splitPageResults($query, $page, $max_rows, $count_key = '*') 
+	function splitPageResults($query, $page, $max_rows, $count_key = '*') 
 	{
-      $this->sql_query = $query;
+		$this->sql_query = $query;
 
-      if (empty($page) || (is_numeric($page) == false)) $page = 1;
-      $this->current_page_number = $page;
+		if (empty($page) || (is_numeric($page) == false))
+			$page = 1;
 
-      $this->number_of_rows_per_page = $max_rows;
+		$this->current_page_number = $page;
 
-      $pos_to = strlen($this->sql_query);
-      $pos_from = strpos($this->sql_query, ' FROM', 0);
+		$this->number_of_rows_per_page = $max_rows;
 
-      $pos_group_by = strpos($this->sql_query, ' GROUP BY', $pos_from);
-      if (($pos_group_by < $pos_to) && ($pos_group_by != false)) $pos_to = $pos_group_by;
+		$pos_to = strlen($this->sql_query);
+		$pos_from = strpos($this->sql_query, ' FROM', 0);
 
-      $pos_having = strpos($this->sql_query, ' HAVING', $pos_from);
-      if (($pos_having < $pos_to) && ($pos_having != false)) $pos_to = $pos_having;
+		$pos_group_by = strpos($this->sql_query, ' GROUP BY', $pos_from);
 
-      $pos_order_by = strpos($this->sql_query, ' ORDER BY', $pos_from);
-      if (($pos_order_by < $pos_to) && ($pos_order_by != false)) $pos_to = $pos_order_by;
+		if (($pos_group_by < $pos_to) && ($pos_group_by != false))
+			$pos_to = $pos_group_by;
 
-      if (strpos($this->sql_query, 'DISTINCT') || strpos($this->sql_query, 'GROUP BY')) {
-        $count_string = 'DISTINCT ' . os_db_input($count_key);
+		$pos_having = strpos($this->sql_query, ' HAVING', $pos_from);
 
-      } else {
-        $count_string = os_db_input($count_key);
-      }
+		if (($pos_having < $pos_to) && ($pos_having != false))
+			$pos_to = $pos_having;
 
-      $count_query = osDBquery($query);
-      $count = os_db_num_rows($count_query,true);
+		$pos_order_by = strpos($this->sql_query, ' ORDER BY', $pos_from);
+		if (($pos_order_by < $pos_to) && ($pos_order_by != false)) $pos_to = $pos_order_by;
 
-      $this->number_of_rows = $count;
-      $this->number_of_pages = ceil($this->number_of_rows / $this->number_of_rows_per_page);
+		if (strpos($this->sql_query, 'DISTINCT') || strpos($this->sql_query, 'GROUP BY'))
+			$count_string = 'DISTINCT '.os_db_input($count_key);
+		else
+			$count_string = os_db_input($count_key);
 
-      if ($this->current_page_number > $this->number_of_pages) {
-        $this->current_page_number = $this->number_of_pages;
-      }
+		$count_query = osDBquery($query);
+		$count = os_db_num_rows($count_query,true);
 
-      $offset = ($this->number_of_rows_per_page * ($this->current_page_number - 1));
+		$this->number_of_rows = $count;
+		$this->number_of_pages = ceil($this->number_of_rows / $this->number_of_rows_per_page);
 
-      $this->sql_query .= " LIMIT " . max($offset, 0) . ", " . $this->number_of_rows_per_page;
+		if ($this->current_page_number > $this->number_of_pages)
+		{
+			$this->current_page_number = $this->number_of_pages;
+		}
 
-    }
+		$offset = ($this->number_of_rows_per_page * ($this->current_page_number - 1));
 
-    function display_links($max_page_links, $parameters = '', $page = 'page') {
-      global $PHP_SELF, $request_type;
+		$this->sql_query .= " LIMIT ".max($offset, 0).", ".$this->number_of_rows_per_page;
+	}
 
-      $display_links_string = '';
+	function display_links($max_page_links, $parameters = '', $page = 'page')
+	{
+		global $PHP_SELF, $request_type;
+		$tpl = new osTemplate;
 
-      $class = 'class="pageResults"';
+		if (os_not_null($parameters) && (substr($parameters, -1) != '&'))
+			$parameters .= '&';
 
-      if (os_not_null($parameters) && (substr($parameters, -1) != '&')) $parameters .= '&';
+		$prev = array();
+		if ($this->current_page_number > 1)
+		{
+			$prev = array(
+				'title' => PREVNEXT_BUTTON_PREV,
+				'link' => os_href_link(basename($PHP_SELF), $parameters .$page. '='.($this->current_page_number - 1), $request_type),
+			);
+		}
 
-      if ($this->current_page_number > 1) $display_links_string .= '<a href="' . os_href_link(basename($PHP_SELF), $parameters .$page. '=' . ($this->current_page_number - 1), $request_type) . '" class="pageResults" title=" ' . PREVNEXT_TITLE_PREVIOUS_PAGE . ' ">' . PREVNEXT_BUTTON_PREV . '</a>&nbsp;&nbsp;';
+		$cur_window_num = intval($this->current_page_number / $max_page_links);
 
-      $cur_window_num = intval($this->current_page_number / $max_page_links);
-      if ($this->current_page_number % $max_page_links) $cur_window_num++;
+		if ($this->current_page_number % $max_page_links)
+			$cur_window_num++;
 
-      $max_window_num = intval($this->number_of_pages / $max_page_links);
-      if ($this->number_of_pages % $max_page_links) $max_window_num++;
+		$max_window_num = intval($this->number_of_pages / $max_page_links);
 
-      if ($cur_window_num > 1) $display_links_string .= '<a href="' . os_href_link(basename($PHP_SELF), $parameters .$page. '=' . (($cur_window_num - 1) * $max_page_links), $request_type) . '" class="pageResults" title=" ' . sprintf(PREVNEXT_TITLE_PREV_SET_OF_NO_PAGE, $max_page_links) . ' ">...</a>';
+		if ($this->number_of_pages % $max_page_links)
+			$max_window_num++;
 
-      for ($jump_to_page = 1 + (($cur_window_num - 1) * $max_page_links); ($jump_to_page <= ($cur_window_num * $max_page_links)) && ($jump_to_page <= $this->number_of_pages); $jump_to_page++) {
-        if ($jump_to_page == $this->current_page_number) {
-          $display_links_string .= '&nbsp;<b>' . $jump_to_page . '</b>&nbsp;';
-        } else {
-          $display_links_string .= '&nbsp;<a href="' . os_href_link(basename($PHP_SELF), $parameters .$page. '=' . $jump_to_page, $request_type) . '" class="pageResults" title=" ' . sprintf(PREVNEXT_TITLE_PAGE_NO, $jump_to_page) . ' ">' . $jump_to_page . '</a>&nbsp;';
-        }
-      }
+		$prev_cur = array();
+		if ($cur_window_num > 1)
+		{
+			$prev_cur = array(
+				'title' => '...',
+				'link' => os_href_link(basename($PHP_SELF), $parameters .$page. '='.(($cur_window_num - 1) * $max_page_links), $request_type),
+			);
+		}
 
-      if ($cur_window_num < $max_window_num) $display_links_string .= '<a href="' . os_href_link(basename($PHP_SELF), $parameters .$page. '=' . (($cur_window_num) * $max_page_links + 1), $request_type) . '" class="pageResults" title=" ' . sprintf(PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE, $max_page_links) . ' ">...</a>&nbsp;';
+		$pagination = array();
+		for ($jump_to_page = 1 + (($cur_window_num - 1) * $max_page_links); ($jump_to_page <= ($cur_window_num * $max_page_links)) && ($jump_to_page <= $this->number_of_pages); $jump_to_page++)
+		{
+			$link = ($jump_to_page == $this->current_page_number) ? '' : os_href_link(basename($PHP_SELF), $parameters .$page. '='.$jump_to_page, $request_type);
+			$pagination[] = array(
+				'title' => $jump_to_page,
+				'link' => $link,
+			);
+		}
 
-      if (($this->current_page_number < $this->number_of_pages) && ($this->number_of_pages != 1)) $display_links_string .= '&nbsp;<a href="' . os_href_link(basename($PHP_SELF), $parameters .$page. '=' . ($this->current_page_number + 1), $request_type) . '" class="pageResults" title=" ' . PREVNEXT_TITLE_NEXT_PAGE . ' ">' . PREVNEXT_BUTTON_NEXT . '</a>&nbsp;';
+		$next_cur = array();
+		if ($cur_window_num < $max_window_num)
+		{
+			$next_cur = array(
+				'title' => '...',
+				'link' => os_href_link(basename($PHP_SELF), $parameters .$page. '='.(($cur_window_num) * $max_page_links + 1), $request_type).'" class="pageResults" title=" '.sprintf(PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE, $max_page_links),
+			);
+		}
 
-      return $display_links_string;
-    }
+		$next = array();
+		if (($this->current_page_number < $this->number_of_pages) && ($this->number_of_pages != 1))
+		{
+			$next = array(
+				'title' => PREVNEXT_BUTTON_NEXT,
+				'link' => os_href_link(basename($PHP_SELF), $parameters .$page. '='.($this->current_page_number + 1), $request_type),
+			);
+		}
 
-    function display_count($text_output) {
-      $to_num = ($this->number_of_rows_per_page * $this->current_page_number);
-      if ($to_num > $this->number_of_rows) $to_num = $this->number_of_rows;
+		// Передаем в файл шаблона pagination.html
+		$tpl->assign('prev_cur', $prev_cur);
+		$tpl->assign('prev', $prev);
+		$tpl->assign('next_cur', $next_cur);
+		$tpl->assign('next', $next);
+		$tpl->assign('pagination', $pagination);
 
-      $from_num = ($this->number_of_rows_per_page * ($this->current_page_number - 1));
+		$tpl->assign('pages', $this->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS));
 
-      if ($to_num == 0) {
-        $from_num = 0;
-      } else {
-        $from_num++;
-      }
+		// Файл шаблона не кэшируем
+		$tpl->caching = 0;
+		$getTemplate = $tpl->fetch(CURRENT_TEMPLATE.'/module/pagination.html');
 
-      return sprintf($text_output, $from_num, $to_num, $this->number_of_rows);
-    }
-  }
-?>
+		return $getTemplate;
+	}
+
+	function display_count($text_output)
+	{
+		$to_num = ($this->number_of_rows_per_page * $this->current_page_number);
+
+		if ($to_num > $this->number_of_rows)
+			$to_num = $this->number_of_rows;
+
+		$from_num = ($this->number_of_rows_per_page * ($this->current_page_number - 1));
+
+		if ($to_num == 0)
+			$from_num = 0;
+		else
+			$from_num++;
+
+		return sprintf($text_output, $from_num, $to_num, $this->number_of_rows);
+	}
+}
