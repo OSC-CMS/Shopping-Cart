6eba541708feff8d6d2e42836153ed4a8527d159
John <osccms.service@gmail.com>
2014-10-09 23:59:19 +0400

    Бокс "Категории на главной"

---
+++ modules/plugins/box_categories_main/box_categories_main.php
@@ -0,0 +1,54 @@
+<?php
+/*
+	Plugin Name: Категории на главной
+	Plugin URI: http://osc-cms.com/store/plugins/box-categories-main
+	Version: 1.0
+	Description: Выводит блок категорий на главной
+	Author: CartET
+	Author URI: http://osc-cms.com
+	Plugin Group: Boxes
+*/
+
+add_action('box', 'box_categories_main_func');
+add_filter('head_array_detail', 'box_categories_main_css');
+
+function box_categories_main_css($value)
+{
+	$theme = (file_exists(plugdir().'themes/'.CURRENT_TEMPLATE)) ? CURRENT_TEMPLATE : 'default';
+	add_style(plugurl().'themes/'.$theme.'/css/box_categories_main.css', $value, 'categories_main');
+
+	return $value;
+}
+
+function box_categories_main_func()
+{
+	global $osTemplate, $cartet;
+	$box = new osTemplate;
+
+	$box->assign('aCategories', $cartet->product->getCategoriesTree());
+	$box->assign('current', $cartet->product->getCurrentCategory());
+	$box->assign('current_in', $cartet->product->getCurrentCategory(true));
+	$box->assign('image', get_option('showCatImagesMain'));
+	$box->assign('imageWidth', get_option('cImgWidthMain'));
+	$box->assign('imageHeight', get_option('cImgHeightMain'));
+	$box->assign('counts', get_option('countProductsMain'));
+
+	$theme = (file_exists(plugdir().'themes/'.CURRENT_TEMPLATE)) ? CURRENT_TEMPLATE : 'default';
+
+	$box->assign('plugDir', dirname(__FILE__).'/themes/'.$theme);
+	$box->assign('language', $_SESSION['language']);
+	$box->template_dir = plugdir();
+
+	$box->caching = 0;
+	$_box_value = $box->fetch(dirname(__FILE__).'/themes/'.$theme.'/categories.html');
+	$osTemplate->assign('box_CATEGORIES_MAIN', $_box_value);
+}
+
+function box_categories_main_install()
+{
+	add_option('countProductsMain',		'false', 'radio', "array('true', 'false')");
+	add_option('subCategoriesMain',		'true', 'radio', "array('true', 'false')");
+	add_option('showCatImagesMain',		'false', 'radio', "array('true', 'false')");
+	add_option('cImgWidthMain',			'30', 'input');
+	add_option('cImgHeightMain',		'30', 'input');
+}
---
+++ modules/plugins/box_categories_main/ru.php
@@ -0,0 +1,18 @@
+<?php
+/*
+*---------------------------------------------------------
+*
+*	CartET - Open Source Shopping Cart Software
+*	http://www.cartet.org
+*
+*---------------------------------------------------------
+*/
+
+$lang['countProductsMain']		= 'Счетчик товара';
+$lang['subCategoriesMain']		= 'Вывод подкатегорий';
+$lang['showCatImagesMain']		= 'Вывод изображения категорий';
+$lang['cImgWidthMain']			= 'Ширина изображения';
+$lang['cImgHeightMain']			= 'Высота изображения';
+
+$lang['true']		= 'Да';
+$lang['false']		= 'Нет';
