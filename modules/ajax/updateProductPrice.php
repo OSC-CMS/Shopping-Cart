5b81a7c02ade33d445521d3b98c19e43a9bbf583
John <osccms.service@gmail.com>
2014-11-08 11:12:26 +0400

    фикс пересчета цены в карточке товара

--- modules/ajax/updateProductPrice.php
+++ modules/ajax/updateProductPrice.php
@@ -16,6 +16,9 @@ if (isset($_POST['products_id']) && !empty($_POST['products_id']))
 
 	$pPrice = $osPrice->GetPrice($oProduct->data['products_id'], false, 1, $oProduct->data['products_tax_class_id'], $oProduct->data['products_price'], 1, 0, $oProduct->data['products_discount_allowed']);
 
+	$products_qty = str_replace('-', '', (int)$_POST['products_qty']);
+	$products_qty = ($products_qty > 0) ? $products_qty : 1;
+
 	$attrPrice = 0;
 	if (isset($_POST['id']))
 	{
@@ -26,7 +29,7 @@ if (isset($_POST['products_id']) && !empty($_POST['products_id']))
 		}
 	}
 
-	$total = $pPrice['price']*$_POST['products_qty']+$attrPrice*$_POST['products_qty'];
+	$total = $pPrice['price']*$products_qty+$attrPrice*$products_qty;
 	$result = $osPrice->Format($total, true);
 
 	echo json_encode($result);
