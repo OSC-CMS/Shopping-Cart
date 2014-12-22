08314b5221b81a65313ac520324bb350a347e2fc
John <osccms.service@gmail.com>
2014-12-22 08:55:22 +0400

    корректировка вывода рекомендуемых и новинок

--- modules/featured.php
+++ modules/featured.php
@@ -15,71 +15,61 @@
 
 $module = new osTemplate;
 
-//fsk18 lock
-$fsk_lock = '';
-if ($_SESSION['customers_status']['customers_fsk18_display'] == '0')
-	$fsk_lock = ' and p.products_fsk18!=1';
-
-if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0')) {
-	if (GROUP_CHECK == 'true')
-		$group_check = " and p.group_permission_".$_SESSION['customers_status']['customers_status_id']."=1 ";
-
-	$featured_products_query = "SELECT distinct * FROM
-	                                         ".TABLE_PRODUCTS." p left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on pd.products_id = p.products_id,
-	                                         ".TABLE_FEATURED." f where
-	                                         p.products_id=f.products_id ".$group_check."
-	                                         ".$fsk_lock."
-	                                         and p.products_status = '1' and f.status = '1' and pd.language_id = '".(int) $_SESSION['languages_id']."'
-	                                         order by p.products_date_added DESC limit ".MAX_DISPLAY_FEATURED_PRODUCTS;
-} else {
-
-	if (GROUP_CHECK == 'true')
-		$group_check = "and p.group_permission_".$_SESSION['customers_status']['customers_status_id']."=1 ";
-
-	$featured_products_query = "SELECT distinct * FROM
-	                                         ".TABLE_PRODUCTS." p left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on pd.products_id = p.products_id,
-	                                         ".TABLE_FEATURED." f,
-	                                        ".TABLE_PRODUCTS_TO_CATEGORIES." p2c,
-	                                        ".TABLE_CATEGORIES." c
-	                                        where c.categories_status='1'
-	                                        and p.products_id = p2c.products_id and p.products_id=f.products_id
-	                                        and p2c.categories_id = c.categories_id
-	                                        ".$group_check."
-	                                        ".$fsk_lock."
-	                                        and c.parent_id = '".$featured_products_category_id."'
-	                                        and p.products_status = '1' and f.status = '1' and pd.language_id = '".(int) $_SESSION['languages_id']."'
-	                                        order by p.products_date_added DESC limit ".MAX_DISPLAY_FEATURED_PRODUCTS;
+if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0'))
+{
+	$featured_products_query = $cartet->product->getList(array(
+		'products_status' => 1,
+		'category_status' => 1,
+		'group' => 'p.products_id',
+		'where' => array('f.products_id = p.products_id', 'f.status = 1'),
+		'order' => 'p.products_date_added DESC',
+		'limit' => MAX_DISPLAY_FEATURED_PRODUCTS,
+	));
 }
-$row = 0;
+else
+{
+	$featured_products_query = $cartet->product->getList(array(
+		'products_status' => 1,
+		'category_status' => 1,
+		'group' => 'p.products_id',
+		'where' => array('f.products_id = p.products_id', 'f.status = 1', 'c.parent_id = '.$featured_products_category_id),
+		'order' => 'p.products_date_added DESC',
+		'limit' => MAX_DISPLAY_FEATURED_PRODUCTS,
+	));
+}
+
 $module_content = array ();
 $featured_products_query = osDBquery($featured_products_query);
-while ($featured_products = os_db_fetch_array($featured_products_query, true)) {
+while ($featured_products = os_db_fetch_array($featured_products_query, true))
+{
 	$module_content[] = $product->buildDataArray($featured_products);
-
 }
-if (sizeof($module_content) >= 1) {
-   $module->assign('FEATURED_LINK', os_href_link(FILENAME_FEATURED));
+
+if (sizeof($module_content) >= 1)
+{
+	$module->assign('FEATURED_LINK', os_href_link(FILENAME_FEATURED));
 	$module->assign('language', $_SESSION['language']);
 	$module->assign('module_content', $module_content);
 
-	 if (!CacheCheck()) {
+	if (!CacheCheck())
+	{
 		$module->caching = 0;
-		if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0')) {
+		if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0'))
 			$module = $module->fetch(CURRENT_TEMPLATE.'/module/featured_products_default.html');
-		} else {
+		else
 			$module = $module->fetch(CURRENT_TEMPLATE.'/module/featured_products_category.html');
-		}
-	} else {
+	}
+	else
+	{
 		$module->caching = 1;
 		$module->cache_lifetime = CACHE_LIFETIME;
 		$module->cache_modified_check = CACHE_CHECK;
 		$cache_id = $featured_products_category_id.$_SESSION['language'].$_SESSION['customers_status']['customers_status_name'].$_SESSION['currency'];
-		if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0')) {
+		if ((!isset ($featured_products_category_id)) || ($featured_products_category_id == '0'))
 			$module = $module->fetch(CURRENT_TEMPLATE.'/module/featured_products_default.html', $cache_id);
-		} else {
+		else
 			$module = $module->fetch(CURRENT_TEMPLATE.'/module/featured_products_category.html', $cache_id);
-		}
 	}
+
 	$default->assign('MODULE_featured_products', $module);
 }
-?>
--- modules/new_products.php
+++ modules/new_products.php
@@ -15,87 +15,70 @@
 
 $module = new osTemplate;
 
-$fsk_lock = '';
-if ($_SESSION['customers_status']['customers_fsk18_display'] == '0')
-$fsk_lock = ' and p.products_fsk18!=1';
-
 if ((!isset($new_products_category_id)) || ($new_products_category_id == '0'))
 {
-	if (GROUP_CHECK == 'true')
-		$group_check = " and p.group_permission_".$_SESSION['customers_status']['customers_status_id']."=1 ";
-
-	$new_products_query = "SELECT * FROM
-	".TABLE_PRODUCTS." p,
-	".TABLE_PRODUCTS_DESCRIPTION." pd WHERE
-	p.products_id=pd.products_id and
-	p.products_startpage = '1'
-	".$group_check."
-	".$fsk_lock."
-	and p.products_status = '1' and pd.language_id = '".(int) $_SESSION['languages_id']."'
-	order by p.products_startpage_sort ASC limit ".MAX_DISPLAY_NEW_PRODUCTS;
+	$new_products_query = $cartet->product->getList(array(
+		'products_status' => 1,
+		'category_status' => 1,
+		'group' => 'p.products_id',
+		'where' => array('p.products_startpage = 1'),
+		'order' => 'p.products_startpage_sort ASC',
+		'limit' => MAX_DISPLAY_NEW_PRODUCTS,
+	));
 } 
 else 
 {
-	if (GROUP_CHECK == 'true')
-		$group_check = "and p.group_permission_".$_SESSION['customers_status']['customers_status_id']."=1 ";
-
 	if (MAX_DISPLAY_NEW_PRODUCTS_DAYS != '0')
 	{
 		$date_new_products = date("Y.m.d", mktime(1, 1, 1, date(m), date(d) - MAX_DISPLAY_NEW_PRODUCTS_DAYS, date(Y)));
-		$days = " and p.products_date_added > '".$date_new_products."' ";
+		$where[] = "p.products_date_added > '".$date_new_products."'";
 	}
 
-	$new_products_query = "
-	SELECT
-		*
-	FROM
-		".TABLE_PRODUCTS." p
-			left join ".TABLE_PRODUCTS_DESCRIPTION." pd on (p.products_id = pd.products_id and pd.language_id = '".(int) $_SESSION['languages_id']."')
-			left join ".TABLE_PRODUCTS_TO_CATEGORIES." p2c on (p.products_id = p2c.products_id)
-			left join ".TABLE_CATEGORIES." c on (p2c.categories_id = c.categories_id and c.categories_status = '1')
-	WHERE
-		p.products_status = '1' AND
-		c.parent_id = '".$new_products_category_id."'
-		".$group_check."
-		".$fsk_lock."
-	ORDER BY
-		p.products_date_added DESC
-	LIMIT
-		".MAX_DISPLAY_NEW_PRODUCTS;
+	$where[] = 'p.products_startpage = 1';
+	$where[] = 'c.parent_id = '.$new_products_category_id;
 
-
+	$new_products_query = $cartet->product->getList(array(
+		'products_status' => 1,
+		'category_status' => 1,
+		'group' => 'p.products_id',
+		'where' => $where,
+		'order' => 'p.products_date_added DESC',
+		'limit' => MAX_DISPLAY_NEW_PRODUCTS,
+	));
 }
-$row = 0;
+
 $module_content = array ();
 $new_products_query = osDBquery($new_products_query);
 while ($new_products = os_db_fetch_array($new_products_query, true)) 
 {
-$module_content[] = $product->buildDataArray($new_products);
-}
-if (sizeof($module_content) >= 1) {
-$module->assign('NEW_PRODUCTS_LINK', os_href_link(FILENAME_PRODUCTS_NEW));
-$module->assign('language', $_SESSION['language']);
-$module->assign('module_content', $module_content);
-
-if (!CacheCheck()) {
-$module->caching = 0;
-if ((!isset ($new_products_category_id)) || ($new_products_category_id == '0')) {
-$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_default.html');
-} else {
-$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_category.html');
-}
-} else {
-$module->caching = 1;
-$module->cache_lifetime = CACHE_LIFETIME;
-$module->cache_modified_check = CACHE_CHECK;
-$cache_id = $new_products_category_id.$_SESSION['language'].$_SESSION['customers_status']['customers_status_name'].$_SESSION['currency'];
-if ((!isset ($new_products_category_id)) || ($new_products_category_id == '0')) {
-$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_default.html', $cache_id);
-} else {
-$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_category.html', $cache_id);
-}
-}
-$default->assign('MODULE_new_products', $module);
+	$module_content[] = $product->buildDataArray($new_products);
 }
 
-?>
+if (sizeof($module_content) >= 1)
+{
+	$module->assign('NEW_PRODUCTS_LINK', os_href_link(FILENAME_PRODUCTS_NEW));
+	$module->assign('language', $_SESSION['language']);
+	$module->assign('module_content', $module_content);
+
+	if (!CacheCheck())
+	{
+		$module->caching = 0;
+		if ((!isset ($new_products_category_id)) || ($new_products_category_id == '0'))
+			$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_default.html');
+		else
+			$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_category.html');
+	}
+	else
+	{
+		$module->caching = 1;
+		$module->cache_lifetime = CACHE_LIFETIME;
+		$module->cache_modified_check = CACHE_CHECK;
+		$cache_id = $new_products_category_id.$_SESSION['language'].$_SESSION['customers_status']['customers_status_name'].$_SESSION['currency'];
+		if ((!isset ($new_products_category_id)) || ($new_products_category_id == '0'))
+			$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_default.html', $cache_id);
+		else
+			$module = $module->fetch(CURRENT_TEMPLATE.'/module/new_products_category.html', $cache_id);
+	}
+
+	$default->assign('MODULE_new_products', $module);
+}
